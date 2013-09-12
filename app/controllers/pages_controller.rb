5def78bc7b33c8f58bf3f0a6e63f77ca6ed12c78
Steven Wanderski <steven@bxcreative.com>
2013-09-12 12:03:53 -0500

    dev push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   end
 
   def poses
-    @poses = Pose.gender(params[:gender]).nude(params[:nude]).limit(3)
+    @poses = Pose.gender(params[:gender]).nude(params[:nude])
     @poses = @poses.shuffle
     @duration = params[:duration].to_i * 1000
   end
