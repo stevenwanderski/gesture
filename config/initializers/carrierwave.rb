4fd5fe6ac1370915063c637999d54690a5c6262a
Steven Wanderski <steven@bxcreative.com>
2013-09-14 08:51:29 -0500

    carrierwave config file

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['S3_KEY'],
+    :aws_secret_access_key  => ENV['S3_SECRET'],
+  }
+  config.fog_directory  = 'bxcreative'                     # required
+  # config.fog_public     = false                                   # optional, defaults to true
+  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
