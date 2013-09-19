94121e55a2ad5d65d260469508c1c1c6f0b9f359
Steven Wanderski <steven@bxcreative.com>
2013-09-18 19:21:35 -0500

    random order, limit 50

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   end
 
   def poses
-    @poses = Pose.gender(params[:gender]).nude(params[:nude])
+    @poses = Pose.gender(params[:gender]).nude(params[:nude]).limit(50).order('RAND()')
     @poses = @poses.shuffle
     @pose1 = @poses.shift
     @pose2 = @poses.shift
