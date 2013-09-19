6dadabd36102ba37f2733e61f470b83157c969fd
Steven Wanderski <steven@bxcreative.com>
2013-09-18 19:06:51 -0500

    swipe init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require flatui-radio
+//= require swipe
 //= require gesture
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
   def poses
     @poses = Pose.gender(params[:gender]).nude(params[:nude])
     @poses = @poses.shuffle
+    @pose1 = @poses.shift
+    @pose2 = @poses.shift
     @duration = params[:duration].to_i * 1000
   end
 end
